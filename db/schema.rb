b1e262759b9e1cb31ef04186b5671625053e2b27
DeveloperDemetri <developerdemetri@gmail.com>
2016-09-26 08:55:32 -0700

    starting the users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926041701) do
+ActiveRecord::Schema.define(version: 20160926153343) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "user",       null: false
@@ -22,8 +22,16 @@ ActiveRecord::Schema.define(version: 20160926041701) do
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title",      null: false, maximum: 255
+    t.string   "title",      null: false
     t.string   "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "username",   null: false
+    t.string   "salt"
+    t.string   "password",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
